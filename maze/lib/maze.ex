3ff41a50203e8afe390c7cca3355a15111105b4b
Chris Wilding <hello@chriswilding.co.uk>
2017-12-05 20:01:21 +0000

    AoC - Day 5 - A Maze of Twisty Trampolines, All Alike (use Erlang array)

--- maze/lib/maze.ex
+++ maze/lib/maze.ex
@@ -8,22 +8,24 @@ defmodule Maze do
 
   ## Examples
 
-      iex> Maze.steps([0, 3, 0, 1, -3], &(&1 + 1))
-      5
+  iex> Maze.steps([0, 3, 0, 1, -3], &(&1 + 1))
+  5
   """
   def steps(offsets, update) do
-    jump(offsets, 0, 0, update)
+    array = offsets
+            |> Enum.with_index
+            |> Enum.reduce(:array.from_list(offsets), fn({n, index}, array) -> :array.set(index, n, array) end)
+
+    jump(array, 0, 0, :array.size(array), update)
   end
 
-  def jump(offsets, position, count, _) when position >= length(offsets) do
+  def jump(_, position, count, len, _) when position >= len do
     count
   end
 
-  def jump(offsets, position, count, update) do
-    current = Enum.at(offsets, position)
-
-    new_offsets = List.update_at(offsets, position, update)
-
-    jump(new_offsets, position + current, count + 1, update)
+  def jump(offsets, position, count, len, update) do
+    current = :array.get(position, offsets)
+    new_offsets = :array.set(position, update.(:array.get(position, offsets)), offsets)
+    jump(new_offsets, position + current, count + 1, len, update)
   end
 end
